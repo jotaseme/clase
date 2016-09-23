a6a9f0add5d8bb27d25a066ac8668e290b1420d1
bn0434 <bn0434@192.168.7.5>
2016-09-23 19:47:47 +0200

    Inicial

---
+++ src/test/java/es/upm/miw/test/FractionTest.java
@@ -0,0 +1,48 @@
+package es.upm.miw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.upm.miw.Fraction;             
+
+public class FractionTest {
+	
+	private Fraction f;
+
+    @Before
+    public void before() {
+        f = new Fraction(4, 2);
+    }
+
+
+	@Test
+	public void testFractionIntInt() {
+		assertEquals(4, f.getNumerator());
+        assertEquals(2, f.getDenominator());
+	}
+
+
+	public void testFraction() {
+		assertEquals(1, f.getNumerator());
+        assertEquals(1, f.getDenominator());
+	}
+
+	@Test
+	public void testDecimal() {
+		assertEquals(2, f.decimal(),10e-5);
+     
+	}
+
+	@Test
+	public void testGetNumerator() {
+		assertEquals(4, f.getNumerator());
+	}
+
+	@Test
+	public void testGetDenominator() {
+		assertEquals(2, f.getDenominator());
+	}
+
+}
